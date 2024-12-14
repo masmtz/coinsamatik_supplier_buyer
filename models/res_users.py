5ab69da657307d69c4ca4d85619d645ef7e4cd6e
masmtz <s.mtz.casillas@gmail.com>
2024-12-14 13:27:08 -0700

    FIX

--- models/res_users.py
+++ models/res_users.py
@@ -9,3 +9,7 @@ class ResUsers(models.Model):
 
     national_purchase = fields.Boolean()
     foreign_purchase = fields.Boolean()
+    has_purchase_groups = fields.Boolean(compute=has_purchase_groups)
+
+    def has_purchase_groups(self):
+        self.has_purchase_groups = self.has_group("purchase.group_warning_purchase") or self.has_group("purchase.group_purchase_manager"
