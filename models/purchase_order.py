50969c9a240ee4818eb6d704bad272bc7ed7f61a
masmtz <s.mtz.casillas@gmail.com>
2024-12-14 12:38:20 -0700

    FIX in views

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -7,7 +7,9 @@ from odoo.exceptions import UserError
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+    current_user_id = fields.Many2one("res.users", default=lambda self: self.env.user)
+
     @api.onchange("partner_id")
     def onchange_partner_id(self):
         if self.partner_id:
-            self.user_id = self.partner_id.purchase_user_id
+            self.user_id = self.partner_id.purchase_user_id or False
