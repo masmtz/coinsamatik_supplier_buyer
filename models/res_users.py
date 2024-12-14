bc5390c4cffe80fd787f003a5db0c1d93083406d
masmtz <s.mtz.casillas@gmail.com>
2024-12-14 13:38:26 -0700

    FIx

--- models/res_users.py
+++ models/res_users.py
@@ -7,11 +7,11 @@ from odoo.exceptions import UserError
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    national_purchase = fields.Boolean()
-    foreign_purchase = fields.Boolean()
-    has_purchase_groups = fields.Boolean(compute="has_purchase_groups")
-
-    def has_purchase_groups(self):
+    def compute_has_purchase_groups(self):
         self.has_purchase_groups = self.has_group(
             "purchase.group_warning_purchase"
         ) or self.has_group("purchase.group_purchase_manager")
+
+    national_purchase = fields.Boolean()
+    foreign_purchase = fields.Boolean()
+    has_purchase_groups = fields.Boolean(compute="compute_has_purchase_groups")
