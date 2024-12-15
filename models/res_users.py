46cc648c4c22ee3d2e487db65c32af4392211206
masmtz <s.mtz.casillas@gmail.com>
2024-12-15 15:56:01 -0700

    [FIX]

--- models/res_users.py
+++ models/res_users.py
@@ -8,9 +8,12 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     def compute_has_purchase_groups(self):
-        self.national_purchase = self.has_group("coinsamatik_supplier_buyer.national_purchase"):
-        self.foreign_purchase = self.has_group("coinsamatik_supplier_buyer.foreign_purchase"):
-            
+        self.national_purchase = self.has_group(
+            "coinsamatik_supplier_buyer.national_purchase"
+        )
+        self.foreign_purchase = self.has_group(
+            "coinsamatik_supplier_buyer.foreign_purchase"
+        )
 
     national_purchase = fields.Boolean(compute="compute_has_purchase_groups")
     foreign_purchase = fields.Boolean(compute="compute_has_purchase_groups")
